39efa5e971f31ec022d532819ec540d3f5e6be3c
bantikonakala <bantikonakala1432@gmail.com>
2026-01-28 23:57:54 +0530

    added conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "suresh :: hi ramesh how are you?"
+echo "ramesh :: im doing good thanks for asking, how about you?"
+echo "suresh :: imalso good ramesh , what are you learning?"
+echo "ramesh :: im learning shell scriptin suresh "
