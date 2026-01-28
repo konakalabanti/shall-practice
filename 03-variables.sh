3f4d3b4359dd77a9f6c58bb7734cd5086f371af9
bantikonakala <bantikonakala1432@gmail.com>
2026-01-29 00:45:56 +0530

    03-variables.sh

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=suresh
+PERSON2=ramesh
+
+echo "$PERSON1 :: hi $PERSON2 how are you?"
+echo "$PERSON2:: im doing good thanks for asking, how about you?"
+echo "$PERSON1 :: imalso good $PERSON2 , what are you learning?"
+echo "$PERSON2 :: im learning shell scriptin $PERSON1 "
