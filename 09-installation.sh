c7972f0746d284e9fc1d2712e267a2705b5860b1
bantikonakala <bantikonakala1432@gmail.com>
2026-01-30 00:39:21 +0530

    actions

---
+++ 09-installation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [$USERID -ne 0]; then
+   echo "please run the script with user user"
+
+   exit 1 #install chese vadu root user kakapothe exit avvu ani 
+fi
+
+echo "Installing Nginx" #for printing
+dnf install nginx -y  #command 
+
+if [ $? -ne 0]; then  #for previous command excute ayindha ha kaledha ane command
+   echo "Installing Nginx ... FAILURE" #for printing
+   ecxir 1
+
+else #failre kakapothe 
+   echo "Installing Nginx ... SUCCESS" #ani evvu
+
+fi
