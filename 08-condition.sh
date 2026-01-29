0ed39b61984868d4827a503653e0b0ae39c46a50
bantikonakala <bantikonakala1432@gmail.com>
2026-01-29 23:28:01 +0530

    actions

--- 08-condition.sh
+++ 08-condition.sh
@@ -4,7 +4,9 @@ NUMBER=$1
    
 
 if [ $NUMBER -gt 20 ]; then
-   echo "given number: $NUMBER  is greater then 20" 
+   echo "given number: $NUMBER  is greater then 20"
+elif [ $NUMBER -eq 20 ]; then
+     echo "given number: $NUMBER  is equal then 20"
 fi
 
 echo "given number: $NUMBER  is less then 20" 
