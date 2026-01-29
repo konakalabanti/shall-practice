d369bb8828521be5c6160dc33bf2ace3375a5786
bantikonakala <bantikonakala1432@gmail.com>
2026-01-29 18:00:32 +0530

    actions

--- 05-date-variables.sh
+++ 05-date-variables.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 
-TIMESTAMP=$(date) #timestamp is variable , date ane command ni dynamnic excute chesi  aa output ni e timestamp lo store chesthundhi 
+START_TIME=$(date +%s) #timestamp is variable , date ane command ni dynamnic excute chesi  aa output ni e timestamp lo store chesthundhi 
 
-echo "script is excuted at: $TIMESTAMP" #for printing 
+echo "script is excuted at: $START_TIME" #for printing
+
+sleep 10 
+
+END_TIME=$(date +%s)
+TOTEL_TIME=$(($END_TIME-$START_TIME))
+
+echo "script excuted at : $TOTEL_TIME"
