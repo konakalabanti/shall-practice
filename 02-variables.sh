3577a69900ae2ba2595963af56e151e1851599e0
bantikonakala <bantikonakala1432@gmail.com>
2026-01-29 16:52:39 +0530

    actions

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=suresh
+PERSON2=ramesh
+
+echo "$PERSON1 :: hello $PERSON2 , how are you"
+echo "$PERSON2:: hi $PERSON1 im doing good"
+echo "$PERSON1 :: what doing $PERSON2"
+echo "$PERSON2 :: learning shall $PERSON1"
