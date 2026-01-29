d77c9b2fba065ba3c9975d3ea3576f3e5bd7ac3c
bantikonakala <bantikonakala1432@gmail.com>
2026-01-29 17:16:02 +0530

    actions

---
+++ 04-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "please enter your user name ::"
+read USER_NAME  #this is variable and command manam please enter user name dhaggara echhedhi USERNAME lo store avuthindhi.
+
+echo "user name $USER_NAME"  # this is for printing purpose
