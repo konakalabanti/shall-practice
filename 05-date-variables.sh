4a634295c4c29eebb0976d78d86d2a42293e2660
bantikonakala <bantikonakala1432@gmail.com>
2026-01-29 17:41:00 +0530

    actions

---
+++ 05-date-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+TIMESTAMP=$(date) #timestamp is variable , date ane command ni dynamnic excute chesi  aa output ni e timestamp lo store chesthundhi 
+
+echo "script is excuted at: $TIMESTAMP" #for printing 
