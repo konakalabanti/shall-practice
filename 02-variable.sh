e60cb8338008ca5b5e594e7f8a6c9d13003f4e62
bantikonakala <bantikonakala1432@gmail.com>
2026-02-12 19:47:14 +0530

    conversation

---
+++ 02-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "suresh :: how are you ramesh"
+echo "ramesh :: im good thank you,how are you"
+echo "suresh :: waht are you doing ramesh"
+echo "ramesh :: learning shell scripting "
