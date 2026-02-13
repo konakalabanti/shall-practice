2ff62325d01711e1742cd86324793f97d369cac6
bantikonakala <bantikonakala1432@gmail.com>
2026-02-13 12:13:19 +0530

    change somthing

--- variable01.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "suresh :: how are you ramesh"
-echo "ramesh :: im good thank you,how are you"
-echo "suresh :: waht are you doing ramesh"
-echo "ramesh :: learning shell scripting "
