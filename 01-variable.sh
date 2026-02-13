04960c225c092b9afc501c0df3db3df9cae1581f
bantikonakala <bantikonakala1432@gmail.com>
2026-02-13 12:50:26 +0530

    change somthing

--- 01-variable.sh
+++ 01-variable.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "suresh :: how are you ramesh"
-echo "ramesh :: im good thank you,how are you"
-echo "suresh :: waht are you doing ramesh"
-echo "ramesh :: learning shell scripting "
+PERSON1=suresh
+PERSON2=ramesh
+
+echo "$PERSON1 :: how are you $PERSON2"
+echo "$PERSON2 :: im good thank you,how are you $PERSON1"
+echo "$PERSON1 :: waht are you doing $PERSON2"
+echo "$PERSON2 :: learning shell scripting $PERSON1 "
