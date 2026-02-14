ec116176a61b16d46323ab0cbaa087f52bf87ace
bantikonakala <bantikonakala1432@gmail.com>
2026-02-14 14:25:35 +0530

    change somthing

---
+++ 07-installnginx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u)
+ 
+ if [ $USERID -ne 0 ] ; then
+   echo "please run the script with root user"
+
+   exit 1
+
+   echo "please install Nginx"
+
+   dnf install nginx -y
+if   
