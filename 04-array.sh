0e180d1ea824cc7ca283d3d6c051105dcce67603
bantikonakala <bantikonakala1432@gmail.com>
2026-02-14 11:28:12 +0530

    change somthing

--- 04-array.sh
+++ 04-array.sh
@@ -3,3 +3,7 @@
 FRUTES=("Apple" "banana" "pomo")
 
 echo "fruites are : ${FRUTES[@]}"
+
+echo "first fruit is : ${FRUTES[0]}"
+echo "second fruit is : ${FRUTES[1]}"
+echo "third fruit is : ${FRUTES[2]}"
