7d9f13e5f80626c1c58588e634c8d58206617b5a
bantikonakala <bantikonakala1432@gmail.com>
2026-02-14 16:54:37 +0530

    change somthing

--- 10-logs.sh
+++ 10-logs.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 USERID=$(id -u)
+FOLDER_LOG="/var/logs/shell-script"
+LOG_FILE="/var/logs/shell-scrip/$0.log
  
  if [ $USERID -ne 0 ] ; then
    echo "please switch to root"
    exit 1
  fi
+
+mkdir -p $FOLDER_LOG
 
  VALIDATE() {
 
@@ -16,18 +20,15 @@ USERID=$(id -u)
      else 
        echo "$2.... SUCCESS"
     fi
-}
+ }
 
-   dnf install nginx -y
+   dnf install nginx -y &>> $LOG_FILE
    VALIDATE $? "installing nginx"
 
    
-   dnf install mysql -y
+   dnf install mysql -y  &>> $LOG_FILE
    VALIDATE $? "installing mysql"
 
 
-   dnf install nodejs -y
+   dnf install nodejs -y  &>> $LOG_FILE
    VALIDATE $? "installing nodejs"
-
-
-   
